ca95704aa093611c57906d4fe9ca8dfcb6cf15c4
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-06-04 18:34:05 +0200

    Setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from "@rollup/plugin-typescript";
+
+
+export default {
+  input: ['./src/index.ts'],
+  output: {
+    format: 'es',
+    dir: './'
+  },
+  plugins: [
+    typescript()
+  ]
+}
