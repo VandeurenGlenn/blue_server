8f4b746b265e0db0ec9648bcd355fb3d69984b8e
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-06-04 18:44:20 +0200

    Setup project files and add dotenv

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
 
-
 export default {
   input: ['./src/index.ts'],
   output: {
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import dotenv from 'dotenv'
+const keys = dotenv.config()
