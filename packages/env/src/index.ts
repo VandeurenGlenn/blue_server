962f5fbf01cea838d7c211e4e5dbb75443df1470
Valentin Degenne <vdegenne@gmail.com>
2023-06-10 15:34:20 +0200

    move env to its own package (#12)

---
+++ packages/env/src/index.ts
@@ -0,0 +1,19 @@
+import {DotEnvKeys} from '@blueserver/types';
+import dotenv from 'dotenv';
+import pathlib from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = pathlib.dirname(fileURLToPath(import.meta.url));
+
+const _env = dotenv.config({
+	path: pathlib.join(__dirname, '..', '.env'),
+}).parsed as unknown as DotEnvKeys | undefined;
+
+// Required to exit the program nicely
+// and remove undefined errors further in the code.
+if (_env === undefined) {
+	console.log('env vars not found');
+	process.exit(0);
+}
+
+export const env = _env as DotEnvKeys;
