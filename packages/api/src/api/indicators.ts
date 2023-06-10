8dd9c942f9f950c6567b01fbd400918461e7fcb2
vdegenne <vdegenne@gmail.com>
2023-06-10 13:06:45 +0200

    just a test

--- packages/api/src/api/indicators.ts
+++ packages/api/src/api/indicators.ts
@@ -3,7 +3,7 @@ import {GithubIndicator, GithubProject} from '@blueserver/types';
 export function buildGithubIndicators(githubProject: GithubProject) {
 	// TODO
 	// ok :D
-	
+	// good
 }
 
 export function buildSuperMegaIndicatorFromAllIndicators(
