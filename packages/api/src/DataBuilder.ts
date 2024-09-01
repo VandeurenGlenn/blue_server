18c3094a0646fbf98c355c5d60803b39cdbbb15b
Valentin Degenne <vdegenne@gmail.com>
2024-09-01 15:28:25 +0200

    Update api dev deps (#32)

--- packages/api/src/DataBuilder.ts
+++ packages/api/src/DataBuilder.ts
@@ -5,112 +5,112 @@ import {GitHub, type GithubProject, type GithubProjectResponse} from './api/gith
 import {SEVEN_DAYS_AGO} from './constants.js'
 
 export type BlueAsset = {
-  id: number
-  website: string | null
-  // sourceCode: string;
-  github: GithubProject
-  indicators: {
-    github: null
-  }
+	id: number
+	website: string | null
+	// sourceCode: string;
+	github: GithubProject
+	indicators: {
+		github: null
+	}
 }
 
 export class DataBuilder {
-  coinMarketCap: CoinMarketCap
-  gitHub: GitHub
+	coinMarketCap: CoinMarketCap
+	gitHub: GitHub
 
-  constructor(keys: {coinmarketcap: string; github: string}) {
-    if (!keys) throw new Error('no api keys found (did you setup env?)')
+	constructor(keys: {coinmarketcap: string; github: string}) {
+		if (!keys) throw new Error('no api keys found (did you setup env?)')
 
-    this.coinMarketCap = new CoinMarketCap(keys.coinmarketcap)
-    this.gitHub = new GitHub(keys.github)
-  }
+		this.coinMarketCap = new CoinMarketCap(keys.coinmarketcap)
+		this.gitHub = new GitHub(keys.github)
+	}
 
-  /**
-   * @param top the number of projects in the top to fetch
-   * @returns {BlueAsset[]} list of blue indicators (for the front end)
-   */
-  async createAssetList(limit = 100): Promise<BlueAsset[]> {
-    const listings: CMCListing[] = await this.coinMarketCap.getLatestListings(limit)
+	/**
+	 * @param top the number of projects in the top to fetch
+	 * @returns {BlueAsset[]} list of blue indicators (for the front end)
+	 */
+	async createAssetList(limit = 100): Promise<BlueAsset[]> {
+		const listings: CMCListing[] = await this.coinMarketCap.getLatestListings(limit)
 
-    const listingsInfo = await this.coinMarketCap.getListingInfo(listings.map((l) => l.id))
+		const listingsInfo = await this.coinMarketCap.getListingInfo(listings.map((l) => l.id))
 
-    // This part is responsible of filling in the blanks (mainly github informations for now)
-    return Promise.all(
-      Object.values(listingsInfo).map(async (asset) => {
-        const blueAsset: BlueAsset = {
-          id: asset.id,
-          website: asset.urls.website[0],
-          // This error is just a demonstration that we don't have
-          // other solution to force consistent types.
-          // sourceCode: asset.urls.source_code[0],
-          github: {
-            activity: {additions: 0, deletions: 0, total: 0},
-            repos: asset.urls.source_code.filter((source) => source.includes('github')),
-            contributers: []
-          },
-          indicators: {
-            github: null
-          }
-        }
-        // A github repo was present in CMC data, we fetch informations
-        if (blueAsset.github.repos.length > 0) {
-          const urlParts = (blueAsset.github.repos[0] as string).replace('https://', '').split('/')
+		// This part is responsible of filling in the blanks (mainly github informations for now)
+		return Promise.all(
+			Object.values(listingsInfo).map(async (asset) => {
+				const blueAsset: BlueAsset = {
+					id: asset.id,
+					website: asset.urls.website[0],
+					// This error is just a demonstration that we don't have
+					// other solution to force consistent types.
+					// sourceCode: asset.urls.source_code[0],
+					github: {
+						activity: {additions: 0, deletions: 0, total: 0},
+						repos: asset.urls.source_code.filter((source) => source.includes('github')),
+						contributers: []
+					},
+					indicators: {
+						github: null
+					}
+				}
+				// A github repo was present in CMC data, we fetch informations
+				if (blueAsset.github.repos.length > 0) {
+					const urlParts = (blueAsset.github.repos[0] as string).replace('https://', '').split('/')
 
-          let repos = await this.gitHub.getRepos(urlParts[1])
-          // TODO(@VandeurenGlenn): isn't that super risky, too much data
-          // @Vdegenne yeah, not ideal, just there till we only return what we really need, then all the rest can go out
-          // @ts-ignore
-          if (repos.length > 0)
-            blueAsset.github.repos = repos.map((repo: GithubProjectResponse) => {
-              return {
-                name: repo.name,
-                full_name: repo.full_name,
-                private: repo.private,
-                url: repo.html_url,
-                description: repo.description,
-                fork: repo.fork,
-                pushed_at: repo.pushed_at,
-                created_at: repo.created_at,
-                size: repo.size,
-                watchers: repo.watchers,
-                forks: repo.forks,
-                visibility: repo.visibility,
-                owner: {
-                  name: repo.owner.name,
-                  avatar: repo.owner.avatar,
-                  gravatar: repo.owner.gravatar,
-                  type: repo.owner.type
-                }
-              } as Partial<GithubProjectResponse>
-            })
+					let repos = await this.gitHub.getRepos(urlParts[1])
+					// TODO(@VandeurenGlenn): isn't that super risky, too much data
+					// @Vdegenne yeah, not ideal, just there till we only return what we really need, then all the rest can go out
+					// @ts-ignore
+					if (repos.length > 0)
+						blueAsset.github.repos = repos.map((repo: GithubProjectResponse) => {
+							return {
+								name: repo.name,
+								full_name: repo.full_name,
+								private: repo.private,
+								url: repo.html_url,
+								description: repo.description,
+								fork: repo.fork,
+								pushed_at: repo.pushed_at,
+								created_at: repo.created_at,
+								size: repo.size,
+								watchers: repo.watchers,
+								forks: repo.forks,
+								visibility: repo.visibility,
+								owner: {
+									name: repo.owner.name,
+									avatar: repo.owner.avatar,
+									gravatar: repo.owner.gravatar,
+									type: repo.owner.type
+								}
+							} as Partial<GithubProjectResponse>
+						})
 
-          let promises = []
+					let promises = []
 
-          for (const repo of repos) {
-            // repo had activity within 7 days, so we try to get it's stats
-            if (new Date(repo.pushed_at).getTime() + SEVEN_DAYS_AGO >= new Date().getTime()) {
-              const [owner, name] = repo.full_name.split('/')
-              promises.push(this.gitHub.getRepositoryCodeFrequency(owner, name))
-            } else {
-              // there was no activity, remove the repo to keep data to front minimal
-              blueAsset.github.repos.splice(repos.indexOf(repo))
-            }
-          }
-          promises = await Promise.all(promises)
-          blueAsset.github.activity = promises.reduce(
-            (previous, current) => {
-              previous.additions += current.additions
-              previous.deletions += current.deletions
-              previous.total += current.additions + current.deletions
-              return previous
-            },
-            {additions: 0, deletions: 0, total: 0}
-          )
-        }
-        return blueAsset
-      })
-    )
-  }
+					for (const repo of repos) {
+						// repo had activity within 7 days, so we try to get it's stats
+						if (new Date(repo.pushed_at).getTime() + SEVEN_DAYS_AGO >= new Date().getTime()) {
+							const [owner, name] = repo.full_name.split('/')
+							promises.push(this.gitHub.getRepositoryCodeFrequency(owner, name))
+						} else {
+							// there was no activity, remove the repo to keep data to front minimal
+							// blueAsset.github.repos.splice(repos.indexOf(repo))
+						}
+					}
+					promises = await Promise.all(promises)
+					blueAsset.github.activity = promises.reduce(
+						(previous, current) => {
+							previous.additions += current.additions
+							previous.deletions += current.deletions
+							previous.total += current.additions + current.deletions
+							return previous
+						},
+						{additions: 0, deletions: 0, total: 0}
+					)
+				}
+				return blueAsset
+			})
+		)
+	}
 }
 
 export const dataBuilder = new DataBuilder(env)
--- packages/api/src/server/http/routes.ts
+++ packages/api/src/server/http/routes.ts
@@ -1,14 +1,13 @@
-import Router from 'koa-router'
+import Router from '@koa/router'
 import {cache} from './../../cache.js'
 
-const router = new Router();
+const router = new Router()
 
-router.get('/top-100', async (ctx) => (ctx.body = cache.bluelist));
+router.get('/top-100', async (ctx) => (ctx.body = cache.bluelist))
 
 export const routes = router.routes()
 
 export const allowedMethods = router.allowedMethods()
 
-export const availableRoutes = [
-  '/top-100'
-]
+export const availableRoutes = ['/top-100']
+
