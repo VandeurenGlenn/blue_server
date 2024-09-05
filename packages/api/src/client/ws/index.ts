5e9731bff7abb74f433335b75b600d811eaad7fd
vdegenne <vdegenne@gmail.com>
2024-09-05 16:06:36 +0200

    Yo

--- packages/api/src/client/ws/index.ts
+++ packages/api/src/client/ws/index.ts
@@ -1,15 +1,20 @@
 import {SocketRequestClient} from 'socket-request-client'
 import {type BlueAsset} from '../../DataBuilder.js'
+<<<<<<< HEAD
 import {type AvailableRoute, WS_PORT} from '../../constants.js'
 import ClientConnection from 'socket-request-client/connection'
 import {type ChangesList} from '../../pubsub.js'
+=======
+import {WS_PORT} from '../../constants.js'
+import ClientConnection from 'socket-request-client/connection'
+>>>>>>> c3cd0ac (Fix socket client import)
 
 export class WSApiClient {
 	#connectionPromise
 	#client: ClientConnection | undefined
 
 	constructor() {
-		const url = !import.meta.env.DEV ? `wss://blue.leofcoin.org` : `ws://localhost:${WS_PORT}`
+		const url = !import.meta.env.DEV ? `ws://blue.leofcoin.org` : `ws://localhost:${WS_PORT}`
 		this.#connectionPromise = new SocketRequestClient(url, 'protocol-blue').init()
 		this.#connectionPromise.then((client) => {
 			this.#client = client
