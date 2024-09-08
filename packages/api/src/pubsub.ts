b83985d77c0e361aee3953fe12fae2920541938a
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2024-09-08 09:25:30 +0200

    Add change1h (#55)
    Co-authored-by: VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>

--- packages/api/src/pubsub.ts
+++ packages/api/src/pubsub.ts
@@ -4,7 +4,7 @@ import {type AvailableRoute} from './constants.js'
 
 export interface ChangesList {
 	id: number
-	change24h: number
+	value: number
 }
 
 declare global {
@@ -21,11 +21,41 @@ export class PubSub {
 	static publishTop100() {
 		pubsub.publish('top100', cache.bluelist)
 	}
-
+	/**
+	 * Publishes the change24h event
+	 * sends daily changes to the client
+	 * should only be used when the client has selected to view the 24h changes
+	 *
+	 * @static
+	 * @memberof PubSub
+	 * @returns {void}
+	 * @example
+	 * PubSub.publishChange24h()
+	 *
+	 */
 	static publishChange24h() {
 		pubsub.publish(
 			'change24h',
-			cache.bluelist.map<ChangesList>((a) => ({id: a.id, change24h: a.change24h}))
+			cache.bluelist.map<ChangesList>((a) => ({id: a.id, value: a.change24h}))
+		)
+	}
+
+	/**
+	 * Publishes the change1h event
+	 * sends hourly changes to the client
+	 * should only be used when the client has selected to view the 1h changes
+	 *
+	 * @static
+	 * @memberof PubSub
+	 * @returns {void}
+	 * @example
+	 * PubSub.publishChange1h()
+	 *
+	 */
+	static publishChange1h() {
+		pubsub.publish(
+			'change1h',
+			cache.bluelist.map<ChangesList>((a) => ({id: a.id, value: a.change1h}))
 		)
 	}
 }
