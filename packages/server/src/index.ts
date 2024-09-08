a2cca53954be0eeb1ccef2d12af4682ff1d91413
vdegenne <vdegenne@gmail.com>
2024-09-08 09:51:19 +0200

    just typo

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,7 +6,7 @@ import {WSApiServer} from './ws/index.js'
 import {init, updateCacheWithRemote, cache} from '@blueserver/api/cache'
 import {PubSub} from '@blueserver/api/pubsub'
 
-// todo: check if cache can be used, if not, update cache & if in dev mode update also
+// TODO: check if cache can be used, if not, update cache & if in dev mode update also
 try {
 	await init()
 	if (cache.lastUpdated + 3600000 < Date.now() || import.meta?.env.DEV) {
@@ -21,7 +21,7 @@ PubSub.publishTop100()
 PubSub.publishChange24h()
 
 // fetch remotely every hour
-// For debugging use something `*/20 * * * * *` = every 20s
+// For debugging use something like `*/20 * * * * *` = every 20s
 const job = new CronJob('0 * * * *', function () {
 	updateCacheWithRemote()
 })
