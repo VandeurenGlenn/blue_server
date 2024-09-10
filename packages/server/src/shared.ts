e3227743195fe8cc04f362f523b8adb937a06c36
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2024-09-10 16:56:39 +0200

    Create shared.ts (#68)
    Co-authored-by: VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>

---
+++ packages/server/src/shared.ts
@@ -0,0 +1,13 @@
+import {cache} from '@blueserver/api/cache'
+
+export const top100 = (): Top100ResponseLoad => {
+	return {list: cache.bluelist, lastUpdated: cache.lastUpdated}
+}
+
+export const change24h = (): ChangesList[] => {
+	return cache.bluelist.map<ChangesList>((a) => ({id: a.id, change: a.change24h}))
+}
+
+export const change1h = (): ChangesList[] => {
+	return cache.bluelist.map<ChangesList>((a) => ({id: a.id, change: a.change1h}))
+}
