675c0f82ff8be1e2bf0d2d64fc97ad873dc6a796
vdegenne <vdegenne@gmail.com>
2024-09-12 03:07:58 +0200

    chore(dialog): tradingview integration test

--- packages/front/src/assetInfo/asset-info-dialog.ts
+++ packages/front/src/assetInfo/asset-info-dialog.ts
@@ -1,26 +1,36 @@
 import type {MdDialog} from '@material/web/all.js';
 import {customElement} from 'custom-element-decorator';
-import {LitElement, html} from 'lit';
+import {LitElement, html, render} from 'lit';
 import {withStyles} from 'lit-with-styles';
 import {state, query} from 'lit/decorators.js';
 import styles from './asset-info-dialog.css?inline';
+import {sleep} from '../utils.js';
 
 @customElement({name: 'asset-info-dialog', inject: true})
 @withStyles(styles)
 export class AssetInfoDialog extends LitElement {
 	@state() open = false;
 	@state() private asset: BlueAsset | undefined = undefined;
+	script;
+	@query('[slot=content]') dialogContentElement!: HTMLFormElement;
 
 	@query('md-dialog') dialog!: MdDialog;
+	@query('#tradingView') tradingViewContainer!: HTMLElement;
 
 	render() {
 		return html`
-			<md-dialog ?open=${this.open} @close=${() => (this.open = false)}>
+			<md-dialog
+				?open=${this.open}
+				@close=${() => (this.open = false)}
+				style="
+	max-width: calc(100vw - 28px);
+	width: 100%;
+	max-height: calc(100vh - 28px);
+	height: 100%;
+	/*--md-dialog-container-shape: 0;*/
+"
+			>
 				${this.asset ? this.#renderDialogContent() : null}
-
-				<div slot="actions">
-					<md-text-button form="form">Close</md-text-button>
-				</div>
 			</md-dialog>
 		`;
 	}
@@ -32,8 +42,23 @@ export class AssetInfoDialog extends LitElement {
 				${this.asset.name}
 			</header>
 
-			<form slot="content" method="dialog" id="form">Coming soon</form>
+			<form slot="content" method="dialog" id="form" style="height:100%">
+				<div id="tradingView" style="height:100%"></div>
+			</form>
+
+			<div slot="actions" style="justify-content:space-between">
+				<md-outlined-button href=${this.asset.website} target="_blank">
+					<md-icon slot="icon">globe</md-icon>
+					${this.asset.website}
+				</md-outlined-button>
+				<div slot="actions"></div>
+				<md-text-button form="form">Close</md-text-button>
+			</div>
 		`;
+	}
+
+	createRenderRoot() {
+		return this;
 	}
 
 	async show(asset?: BlueAsset) {
@@ -41,6 +66,30 @@ export class AssetInfoDialog extends LitElement {
 			this.asset = asset;
 		}
 		this.open = true;
+
+		await this.updateComplete;
+		if (asset) {
+			this.script = document.createElement('script');
+			this.script.type = 'text/javascript';
+			this.script.async = true;
+			this.script.src =
+				'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+			this.script.innerHTML = JSON.stringify({
+				autosize: true,
+				symbol: `${asset.exchanges[0].toUpperCase()}:${asset.symbol}USDT`,
+				interval: 'D',
+				timezone: 'Etc/UTC',
+				theme: 'dark',
+				style: '1',
+				locale: 'en',
+				allow_symbol_change: true,
+				calendar: false,
+				support_host: 'https://www.tradingview.com',
+			});
+			// const dialog = this.renderRoot.querySelector<HTMLElement>('md-dialog');
+			// await this.dialog.updateComplete;
+			render(this.script, this.tradingViewContainer);
+		}
 	}
 
 	close(returnValue?: string) {
