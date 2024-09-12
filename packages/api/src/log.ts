c3da81f0d806e74deec40991f999d00e2d26b5c1
vdegenne <vdegenne@gmail.com>
2024-09-12 21:51:34 +0200

    Looger

--- packages/api/src/log.ts
+++ packages/api/src/log.ts
@@ -1,3 +1,5 @@
+import chalk, {type ChalkInstance} from 'chalk'
+
 let silent = false
 export default {
 	silence() {
@@ -21,5 +23,16 @@ export default {
 			return
 		}
 		console.timeEnd(input)
+	}
+}
+
+export class Logger {
+	constructor(
+		protected name: string,
+		protected color: ChalkInstance
+	) {}
+
+	log(message: string) {
+		console.log(this.color(`[${this.name}] ${message}`))
 	}
 }
