886a50d8664c2c5f4e796a8b591745c3b3b6ba65
vdegenne <vdegenne@gmail.com>
2024-09-13 17:01:06 +0200

    centralize cmc and github

--- packages/api/src/api/github.ts
+++ packages/api/src/api/github.ts
@@ -1,53 +1,137 @@
-import {cache} from './../cache.js'
-import fetch from 'node-fetch'
+import fetch, {type Response} from 'node-fetch'
+import {type CacheFileData, readCacheFile, writeCacheFile} from './../cache.js'
+
+const GITHUB_PROJECTS_FILENAME = 'github-projects.json'
 
 export class GitHub {
 	#headers = new Headers()
+	#projects: CacheFileData<GithubProject[]> | undefined = undefined
 
 	constructor(key: string, apiVersion = '2022-11-28') {
 		this.#headers.append('Authorization', `Bearer ${key}`)
 		this.#headers.append('X-GitHub-Api-Version', apiVersion)
+
+		this.#loadProjects()
 	}
 
-	async getRepositoryCodeFrequency(owner: string, name: string): Promise<GithubActivity> {
-		const headers = this.#headers
-		headers.set('If-None-Match', cache.github.stats.tags[name])
-		const response = await fetch(`https://api.github.com/repos/${owner}/${name}/stats/code_frequency`, {
-			headers: this.#headers
-		})
-		if (response.status === 304) {
-			return cache.github.stats.cached[name]
+	#loadProjects() {
+		this.#projects = readCacheFile(GITHUB_PROJECTS_FILENAME)
+	}
+	#saveProjects() {
+		if (this.#projects === undefined) {
+			throw new Error("Couldn't save. No data to save.")
 		}
-		cache.github.stats.tags[name] = response.headers.get('ETag') as string
-
-		const result: [number, number, number][] = (await response.json()) as []
-		cache.github.stats.cached[name] = {additions: 0, deletions: 0, total: 0}
-		if (result.length > 0) {
-			const [total, additions, deletions] = result[0]
-			cache.github.stats.cached[name] = {total, additions, deletions}
-		}
-		return cache.github.stats.cached[name]
+		this.#projects.lastUpdated = Date.now()
+		writeCacheFile(GITHUB_PROJECTS_FILENAME, this.#projects)
 	}
 
-	async #getRepos(name: string, type: string): Promise<GithubProjectResponse[]> {
+	getProject(cmcAssetId: number) {
+		if (this.#projects === undefined) {
+			throw new Error('Projects cached data not available')
+		}
+		return this.#projects.data.find((project) => project.cmc_id === cmcAssetId)
+	}
+	projectExist(cmcAssetId: number) {
+		return !!this.getProject(cmcAssetId)
+	}
+	#addProject({cmcAssetId, project, force = false}: {cmcAssetId: number; project: GithubProject; force?: boolean}) {
+		this.#projects ??= {lastUpdated: Date.now(), data: []}
+		const _project = this.getProject(cmcAssetId)
+		if (_project === undefined) {
+			this.#projects.data.push(project)
+			this.#saveProjects()
+		} else if (force) {
+			Object.assign(_project, project)
+			this.#saveProjects()
+		}
+	}
+
+	#fetchReposPromise: Promise<GithubProject | undefined> | undefined
+	get fetchReposComplete() {
+		return this.#fetchReposPromise
+	}
+
+	/**
+	 * @param cmcAssetId the id of the CMC asset to bind the repos with
+	 */
+	async #fetchRepos(name: string, type: GitHubRepoType, etag?: string): Promise<Response> {
 		const headers = this.#headers
-		if (cache.github.repos.tags[name]) headers.set('If-None-Match', cache.github.repos.tags[name])
+
+		if (etag) {
+			headers.set('If-None-Match', etag)
+		}
+
 		if (type === 'Organization') {
 			type = 'orgs'
 		}
-		const response = await fetch(`https://api.github.com/${type}/${name}/repos`, {headers})
-		if (response.status === 304) {
-			return cache.github.repos.cached[name]
-		}
-		cache.github.repos.tags[name] = response.headers.get('ETag') as string
-		if (response.status === 404) return []
-		cache.github.repos.cached[name] = (await response.json()) as unknown as GithubProjectResponse[]
-		return cache.github.repos.cached[name]
+
+		return await fetch(`https://api.github.com/${type}/${name}/repos`, {headers})
 	}
 
-	async getRepos(name: string) {
-		let repos = await this.#getRepos(name, 'orgs')
-		if (repos.length === 0) await this.#getRepos(name, 'users')
-		return repos
+	async fetchRepos(name: string, cmcAssetId: number) {
+		if (cmcAssetId === undefined) {
+			throw new Error('Need to provide CMC Id when fetching repos.')
+		}
+		this.#fetchReposPromise = new Promise<GithubProject | undefined>(async (resolve) => {
+			let project = this.getProject(cmcAssetId)
+			let type: GitHubRepoType = 'orgs'
+			let response = await this.#fetchRepos(name, type, project?.etag)
+			if (response.status === 304) {
+				// Unchanged we just return cache
+				resolve(project)
+				return
+			}
+			if (response.status === 404) {
+				response = await this.#fetchRepos(name, type, project?.etag)
+			}
+			if (response.status === 304) {
+				// Unchanged we just return cache
+				resolve(project)
+				return
+			}
+			if (response.status === 404) {
+				// Both request returned nothing, we just return project in cache
+				// but it's more likely this will always be undefined;
+				resolve(project)
+				return
+			}
+
+			// At this point we have a valid response, we can update project object
+			this.#addProject({
+				cmcAssetId,
+				project: {
+					cmc_id: cmcAssetId,
+					type,
+					etag: response.headers.get('ETag')!,
+					repos: (await response.json()) as GithubRepoResponse[]
+				},
+				force: true // force will force replacing if it already exists
+			})
+
+			resolve(project)
+			return
+		})
+
+		return await this.#fetchReposPromise
 	}
+
+	// async getRepositoryCodeFrequency(owner: string, name: string): Promise<GithubActivity> {
+	// 	const headers = this.#headers
+	// 	headers.set('If-None-Match', cache.github.stats.tags[name])
+	// 	const response = await fetch(`https://api.github.com/repos/${owner}/${name}/stats/code_frequency`, {
+	// 		headers: this.#headers
+	// 	})
+	// 	if (response.status === 304) {
+	// 		return cache.github.stats.cached[name]
+	// 	}
+	// 	cache.github.stats.tags[name] = response.headers.get('ETag') as string
+	//
+	// 	const result: [number, number, number][] = (await response.json()) as []
+	// 	cache.github.stats.cached[name] = {additions: 0, deletions: 0, total: 0}
+	// 	if (result.length > 0) {
+	// 		const [total, additions, deletions] = result[0]
+	// 		cache.github.stats.cached[name] = {total, additions, deletions}
+	// 	}
+	// 	return cache.github.stats.cached[name]
+	// }
 }
