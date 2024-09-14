5eca65aadb3cf149ecaa8ef2e842a30375a2bdd9
vdegenne <vdegenne@gmail.com>
2024-09-14 15:03:09 +0200

    just showing test

--- packages/api/src/tickers/ticker-test.ts
+++ packages/api/src/tickers/ticker-test.ts
@@ -1,10 +1,10 @@
 // TODO: remove this file from repo
 import ms from 'ms'
-import {coinMarketCapTicker} from './coinmarketcap.js'
+import {cmcTicker} from './coinmarketcap.js'
 import {createCacheDirectory} from '../cache.js'
 import {githubTicker} from './github.js'
 
 await createCacheDirectory()
-coinMarketCapTicker.startTicker(ms('10s'))
+cmcTicker.startTicker(ms('10s'))
 await new Promise((r) => setTimeout(r, 100))
-githubTicker.startTicker(ms('1s'))
+githubTicker.startTicker(ms('5s'), false)
