20bb01ed75512b4a58288a1f6f5b94aa4a4f5e87
vdegenne <vdegenne@gmail.com>
2024-09-14 15:53:35 +0200

    Update server with new ticker arch

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,31 +1,24 @@
+import {createCacheDirectory} from '@blueserver/api/cache'
 import {PORT, WS_PORT} from '@blueserver/api/constants'
-import {CronJob} from 'cron'
+import {cmcTicker, githubTicker} from '@blueserver/api/tickers'
+import ms from 'ms'
 import {HttpApiServer} from './http/index.js'
 import {WSApiServer} from './ws/index.js'
 
-import {init, updateCacheWithRemote, cache} from '@blueserver/api/cache'
-import {PubSub} from '@blueserver/api/pubsub'
+/**
+ * Some inits
+ */
+// TODO: This probably needs a better place
+await createCacheDirectory()
 
-try {
-	await init()
-	if (cache.lastUpdated + 3600000 < Date.now()) {
-		await updateCacheWithRemote()
-	}
-} catch {
-	// no local data, so we update cache
-	await updateCacheWithRemote()
-}
+/**
+ * Tickers
+ */
+cmcTicker.startTicker(ms('1h'), true)
+githubTicker.startTicker(ms('5s'), true)
 
-PubSub.publishTop100()
-PubSub.publishChange24h()
-PubSub.publishChange1h()
-
-const cronTime = '0 * * * *' // every hour
-// const cronTime = '*/20 * * * * *' // every 20s
-const job = new CronJob(cronTime, function () {
-	updateCacheWithRemote()
-})
-job.start()
-
+/**
+ * Protocols
+ */
 new HttpApiServer({port: PORT})
 new WSApiServer({port: WS_PORT})
